6bd73b29d140636dcde3a11cf7f5f4e6629d3766
Sandipan Kundu <73771749+Codewithsandi@users.noreply.github.com>
2024-05-27 23:53:24 +0530

    P 15

--- 15_Sidebar/js/main.js
+++ 15_Sidebar/js/main.js
@@ -1,20 +1,27 @@
-// fetch("https://openapi.programming-hero.com/api/course/curriculum")
-//   .then((val) => val.json())
-//   .then((data) => console.log(data.data[0].name));
+fetch("https://openapi.programming-hero.com/api/course/curriculum")
+  .then((response) => response.json())
+  .then((data) => {
+    const mileStoneData = data.data;
+    console.log(mileStoneData);
 
-const mileStoneData = JSON.parse(data).data;
+    // Call loadMileStones function here
+    loadMileStones(mileStoneData);
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
 
-console.log(mileStoneData);
-
-function loadMileStones() {
+function loadMileStones(mileStoneData) {
   const mileStones = document.querySelector(".milestones");
   mileStones.innerHTML = `${mileStoneData
     .map(function (milestone) {
       return `
-        <div class="milestone border-b">
+        <div class="milestone border-b" id="${milestone._id}">
             <div class="flex">
-                <div class="checkbox"><input type="checkbox" /></div>
-                <div onclick="openMilestone(this,${milestone._id})">
+                <div class="checkbox"><input type="checkbox" onclick="markMileStone(this, '${
+                  milestone._id
+                }')" /></div>
+                <div onclick="openMilestone(this, '${milestone._id}')">
                     <p>
                     ${milestone.name}
                     <span><i class="fas fa-chevron-down"></i></span>
@@ -38,7 +45,7 @@ function loadMileStones() {
     .join("")}`;
 }
 
-function openMilestone(milestoneElement,id) {
+function openMilestone(milestoneElement, id) {
   const currentPanel = milestoneElement.parentNode.nextElementSibling;
   const shownPanel = document.querySelector(".show");
   const activePanel = document.querySelector(".active");
@@ -53,21 +60,41 @@ function openMilestone(milestoneElement,id) {
   }
   milestoneElement.classList.add("active");
 
-  showMileStone(id)
+  showMileStone(id);
 }
 
-const milestoneImage = document.querySelector(".milestoneImage")
+const milestoneImage = document.querySelector(".milestoneImage");
 function showMileStone(id) {
-    const title = document.querySelector(".title")
-    const details = document.querySelector(".details")
+  const title = document.querySelector(".title");
+  const details = document.querySelector(".details");
 
-    milestoneImage.style.opacity = 0
-    milestoneImage.src = mileStoneData[id].image
-    title.innerText = mileStoneData[id].name
-    details.innerText = mileStoneData[id].description
+  milestoneImage.style.opacity = 0;
+  milestoneImage.src = mileStoneData[id].image;
+  title.innerText = mileStoneData[id].name;
+  details.innerText = mileStoneData[id].description;
 }
-milestoneImage.onload = function(){
-    this.style.opacity = 1
+milestoneImage.onload = function () {
+  this.style.opacity = 1;
+};
+
+function markMileStone(checkbox, id) {
+  const doneList = document.querySelector(".doneList");
+  const milestoneList = document.querySelector(".milestones");
+  const item = document.getElementById(id);
+
+  if (checkbox.checked) {
+    milestoneList.removeChild(item);
+    doneList.appendChild(item);
+    sortList(doneList);
+  } else {
+    doneList.removeChild(item);
+    milestoneList.appendChild(item);
+    sortList(milestoneList);
+  }
 }
 
-loadMileStones();
+function sortList(list) {
+  Array.from(list.children)
+    .sort((a, b) => a.id.localeCompare(b.id, "numeric"))
+    .forEach((node) => list.appendChild(node));
+}
